f06d1576fc23987e0bb28c685e7bd405e3069ce4
GowthamanLL <140971610+GowthamanLL@users.noreply.github.com>
2023-11-02 14:36:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
